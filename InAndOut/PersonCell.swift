c5d6bc5486c7cc4daca52898aef15981da4c6228
Sam Dindyal <sam.dindyal@me.com>
2018-07-19 17:35:50 -0400

    Creates custom cell for displaying a Person object.

---
+++ InAndOut/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  InAndOut
+//
+//  Created by Sam Dindyal on 2018-07-19.
+//  Copyright © 2018 Sam Dindyal. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell:UITableViewCell {
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var addressLabel: UILabel!
+}
